8206ccb1a484777928daf48621149124bf415d0b
WS12 <Admin@WS12s-Mac-mini.local>
2017-04-27 15:49:33 +0530

    1. Added the all fragments and layouts of the Product tab . 1. Added the all fragments and layouts of the Teek Melden tab .

--- app/src/main/java/tekkan/synappz/com/tekkan/fragment/ReportTickFragment.java
+++ app/src/main/java/tekkan/synappz/com/tekkan/fragment/ReportTickFragment.java
@@ -23,8 +23,7 @@ public class ReportTickFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_tick_step1, container, false);
+              return inflater.inflate(R.layout.fragment_report_tick_step1, container, false);
     }
 
 }
